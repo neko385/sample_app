e6ebe5cf28cff26e691b5ab58fe6b65775d9edc9
EC2 Default User <ec2-user@ip-172-31-19-1.ec2.internal>
2022-05-04 07:15:13 +0000

    [Finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,9 +4,9 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    if list.save
-      redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
     else
       render :new
     end
